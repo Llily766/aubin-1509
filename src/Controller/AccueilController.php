246c35893146f6cafaecc18ffdad63ee94b73dc1
BINET aurélie <au.binet21@gmail.com>
2023-11-13 17:00:49 +0100

    ecf

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Services;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager ): Response 
     {
+        $Services = $entityManager->getRepository(Services::class)->findAll();
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'Services' => $Services ,
         ]);
     }
 }
--- src/Controller/OccasionController.php
+++ src/Controller/OccasionController.php
@@ -1,18 +1,21 @@
 <?php
 
 namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Cars;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OccasionController extends AbstractController
 {
     #[Route('/occasion', name: 'app_occasion')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager ): Response 
     {
+        $Cars = $entityManager->getRepository(Cars::class)->findAll();
         return $this->render('occasion/index.html.twig', [
             'controller_name' => 'OccasionController',
+            'Cars' => $Cars,
         ]);
     }
 }
