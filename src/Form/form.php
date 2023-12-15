54953c928cc478b558826ed70a4da3d17c9336b2
BINET aurélie <au.binet21@gmail.com>
2023-12-15 10:53:16 +0100

    ecf

--- src/Form/form.php
+++ src/Form/form.php
@@ -1,26 +1,5 @@
 <?php
 
-namespace App\Controller;
-
-use App\Entity\Cars;
-use App\Entity\Hours;
-use Symfony\Component\DomCrawler\Form;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Test\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-class VoiturController extends AbstractController
-
-
-
-
-
-
-
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
@@ -32,13 +11,12 @@ class AutoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
             ->add('firstname')
             ->add('lastname')
             ->add('phone_number')
-            ->add('email');
+            ->add('email')
+        ;
     }
-
 
     public function configureOptions(OptionsResolver $resolver): void
     {
